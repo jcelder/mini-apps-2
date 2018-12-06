7854857fd28809fc88e32e62b6e3f92c7550f9fc
Joshua Elder <jcelder88@gmail.com>
2018-12-06 11:25:03 -0800

    Get time-series data from API

--- challenge_2/client/components/App.jsx
+++ challenge_2/client/components/App.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+      .then(({ data }) => {
+        this.setState({
+          data,
+        });
+      });
+  }
+
   render() {
     return (
       <div>
         <p>Test</p>
       </div>
-    )
+    );
   }
 }
 
