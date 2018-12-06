e0569148570b7d8af41a453c75c6e4a9dc10ae50
Joshua Elder <jcelder88@gmail.com>
2018-12-05 16:35:56 -0800

    Add boilerplate

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,23 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: [],
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Events go here!</p>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
