97f77941d54fa74c353e079e55d980e7dfd2949a
Joshua Elder <jcelder88@gmail.com>
2018-12-08 09:50:37 -0800

    Create project boilerplate

---
+++ challenge_3/client/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Bowling Scores</p>
+        <p>Controls</p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const morgan = require('morgan');
 
+const port = process.env.PORT || 3000;
+const app = express();
 
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log(`Server listening on port ${3000}`);
+});
